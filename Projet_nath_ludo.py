a07f56fdd4cb513833a89b4e2b8a194f02a59304
mangn <nathan.mang@edu.esiee.fr>
2024-10-14 15:25:31 +0200

    Donnée clean

--- Projet_nath_ludo.py
+++ Projet_nath_ludo.py
@@ -1,14 +1,35 @@
 import pandas as pd
-
+import plotly.offline as pyo
+import numpy as np
+import plotly as plt
+import plotly.express as px
+import seaborn as sns
+import dash
 # Charger le fichier CSV
 file_path = 'Flight_delay.csv'  # Remplacez par le chemin correct si nécessaire
 df = pd.read_csv(file_path)
 
+#Nettoyer les données
+print("Donnée manquante au total: ")
+total_missingValue=df.isna().sum().sum()
+print(total_missingValue)
+print("Pourcentage de donnée manquante: ")
+print((total_missingValue/df.size)*100)
+df_cleanNA=df.dropna()
+
+df_Final=df_cleanNA.drop(['DayOfWeek','Date','UniqueCarrier','FlightNum','TailNum','Cancelled','CancellationCode','Diverted'],axis=1)
+print("Taille du DataFrame nettoyé final")
+print(df_Final.shape)
+print(df_Final.dtypes)
+print(df_Final['Airline'].nunique())
+df_Final=pd.DataFrame(df_Final)
+print(df_Final.head())
+
 # Calculer la moyenne de TaxiIn pour chaque aéroport de destination
-avg_taxi_in_by_dest = df.groupby('Dest')['TaxiIn'].mean()
+avg_taxi_in_by_dest = df_Final.groupby('Dest')['TaxiIn'].mean()
 
 # Calculer la moyenne de TaxiOut pour chaque aéroport d'origine
-avg_taxi_out_by_org = df.groupby('Origin')['TaxiOut'].mean()
+avg_taxi_out_by_org = df_Final.groupby('Origin')['TaxiOut'].mean()
 
 # Classer les moyennes par ordre croissant
 sorted_taxi_in = avg_taxi_in_by_dest.sort_values()
@@ -33,3 +54,5 @@ print(top_10_taxi_out)
 
 print("\nBottom 10 TaxiOut par aéroport d'origine (ordre croissant) :")
 print(bottom_10_taxi_out)
+
+
