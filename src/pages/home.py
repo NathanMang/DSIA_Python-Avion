905cb56e586c08adea5db61defc2d2353199357d
mangn <nathan.mang@edu.esiee.fr>
2024-10-25 22:20:45 +0200

    Histo+Histo Dynamique

--- src/pages/home.py
+++ src/pages/home.py
@@ -1,3 +1,5 @@
+
+
 #
 # Imports
 #
@@ -9,8 +11,10 @@ import dash
 from dash import dcc
 from dash import html
 from dash.dependencies import Input, Output
-from histogramme import creerHist
+from histogramme import creerHist,histogram_Dynamique
+#
 from carte_choroplèthe_px import creerCart
+
 #
 # Main
 #
@@ -19,11 +23,12 @@ if __name__ == '__main__':
 
     app = dash.Dash(__name__) # (3)
 
-    month = 1
-    fig = creerHist(month)
+    
+    fig = creerHist()
+    
     
 
-    carte = creerCart()
+    #carte = creerCart()
 
     
     app.layout = html.Div(children=[
@@ -31,38 +36,42 @@ if __name__ == '__main__':
                             html.H1(children=f'Premier essai',
                                         style={'textAlign': 'center', 'color': '#7FDBFF'}), # (5)
 
-                            html.Label('Month'),
-                            dcc.Slider(
-                                id='month-slider',
-                                min=1,
-                                max=6,
-                                step=1,
-                                value=1
-                            ),
-
+                            
                             dcc.Graph(
                                 id='graph1',
                                 figure=fig
                             ), # (6)
 
-                            dcc.Graph(
-                                id='graph2',
-                                figure=carte
-                            ), # (6)
+                            
+                            # Menu déroulant pour sélectionner le type de retard
+                            dcc.Dropdown(
+                                id='retard-type-dropdown',
+                                options=[
+                                    {'label': 'CarrierDelay - Retard du transporteur', 'value': 'CarrierDelay'},
+                                    {'label': 'WeatherDelay - Retard météorologique', 'value': 'WeatherDelay'},
+                                    {'label': 'NASDelay - Retard du système national d\'aviation', 'value': 'NASDelay'},
+                                    {'label': 'SecurityDelay - Retard de sécurité', 'value': 'SecurityDelay'},
+                                    {'label': 'LateAircraftDelay - Retard d\'arrivée tardive de l\'appareil', 'value': 'LateAircraftDelay'}
+                                ],
+                                value='CarrierDelay',  # Valeur par défaut
+                                clearable=False,
+                                style={'width': '60%', 'margin-bottom': '20px'}
+                            ),
 
+                            # Graphique de l'histogramme
+                            dcc.Graph(id='histogram-graph'),
                             html.Div(children=f'''blablabla'''), # (7)
     ]
     )
 
     @app.callback(
-        Output(component_id='graph1', component_property='figure'),
-        [Input(component_id='month-slider', component_property='value')]
+        Output('histogram-graph', 'figure'),
+        Input('retard-type-dropdown', 'value')
     )
-    def update_figure(input_value):
-        return creerHist(input_value)
-    #
+    def update_histogram(selected_retard_type):
+        return histogram_Dynamique(selected_retard_type)
+    
     # RUN APP
     #
 
     app.run_server(debug=True) # (8)
-
